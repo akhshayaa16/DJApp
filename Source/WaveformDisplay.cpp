99d38f8365c2924d17aeb430e2620dc99f1858fb
Amresh <amresh2000@gmail.com>
2026-02-17 18:26:52 +0800

    Refactor WaveformDisplay constructor and paint method for improved readability

--- Source/WaveformDisplay.cpp
+++ Source/WaveformDisplay.cpp
@@ -12,99 +12,76 @@
 #include "WaveformDisplay.h"
 
 //==============================================================================
-WaveformDisplay::WaveformDisplay(AudioFormatManager & 	formatManagerToUse,
-                                 AudioThumbnailCache & 	cacheToUse) :
-                                 audioThumb(1000, formatManagerToUse, cacheToUse), 
-                                 fileLoaded(false), 
-                                 position(0)
-                          
+WaveformDisplay::WaveformDisplay(AudioFormatManager& formatManagerToUse,
+                                 AudioThumbnailCache& cacheToUse)
+    : audioThumb(1000, formatManagerToUse, cacheToUse),
+      fileLoaded(false),
+      position(0)
 {
-    // In your constructor, you should add any child components, and
-    // initialise any special settings that your component needs.
-
-  audioThumb.addChangeListener(this);
+    audioThumb.addChangeListener(this);
 }
 
 WaveformDisplay::~WaveformDisplay()
 {
 }
 
-void WaveformDisplay::paint (Graphics& g)
+void WaveformDisplay::paint(Graphics& g)
 {
-    /* This demo code just fills the component's background and
-       draws some placeholder text to get you started.
+    g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 
-       You should replace everything in this method with your own
-       drawing code..
-    */
+    g.setColour(Colours::grey);
+    g.drawRect(getLocalBounds(), 1);
 
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));   // clear the background
-
-    g.setColour (Colours::grey);
-    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-
-    g.setColour (Colours::orange);
-    if(fileLoaded)
+    g.setColour(Colours::orange);
+    if (fileLoaded)
     {
-      audioThumb.drawChannel(g, 
-        getLocalBounds(), 
-        0, 
-        audioThumb.getTotalLength(), 
-        0, 
-        1.0f
-      );
-      g.setColour(Colours::lightgreen);
-      g.drawRect(position * getWidth(), 0, getWidth() / 20, getHeight());
+        audioThumb.drawChannel(g,
+            getLocalBounds(),
+            0,
+            audioThumb.getTotalLength(),
+            0,
+            1.0f);
+
+        g.setColour(Colours::lightgreen);
+        g.drawRect(position * getWidth(), 0, getWidth() / 20, getHeight());
     }
-    else 
+    else
     {
-      g.setFont (20.0f);
-      g.drawText ("File not loaded...", getLocalBounds(),
-                  Justification::centred, true);   // draw some placeholder text
-
+        g.setFont(20.0f);
+        g.drawText("File not loaded...", getLocalBounds(),
+                    Justification::centred, true);
     }
 }
 
 void WaveformDisplay::resized()
 {
-    // This method is where you should set the bounds of any child
-    // components that your component contains..
-
 }
 
 void WaveformDisplay::loadURL(URL audioURL)
 {
-  audioThumb.clear();
-  fileLoaded  = audioThumb.setSource(new URLInputSource(audioURL));
-  if (fileLoaded)
-  {
-    std::cout << "wfd: loaded! " << std::endl;
-    repaint();
-  }
-  else {
-    std::cout << "wfd: not loaded! " << std::endl;
-  }
-
+    audioThumb.clear();
+    fileLoaded = audioThumb.setSource(new URLInputSource(audioURL));
+    if (fileLoaded)
+    {
+        std::cout << "wfd: loaded! " << std::endl;
+        repaint();
+    }
+    else
+    {
+        std::cout << "wfd: not loaded! " << std::endl;
+    }
 }
 
-void WaveformDisplay::changeListenerCallback (ChangeBroadcaster *source)
+void WaveformDisplay::changeListenerCallback(ChangeBroadcaster* source)
 {
-    std::cout << "wfd: change received! " << std::endl;
-
     repaint();
-
 }
 
 void WaveformDisplay::setPositionRelative(double pos)
 {
-  if (pos != position)
-  {
-    position = pos;
-    repaint();
-  }
-  
+    if (pos != position)
+    {
+        position = pos;
+        repaint();
+    }
 }
-
-
-
-
